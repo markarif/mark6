07744feb7b4e1cc7d0e5035e263efc4cc5e0e16b
mark <munyweia.mark@gmail.com>
2015-05-14 16:35:15 +0300

    first commit

---
+++ src/main/java/com/springassignment/dao/DrugDao.java
@@ -0,0 +1,16 @@
+package com.springassignment.dao;
+
+import com.springassignment.models.Drug;
+
+import java.util.List;
+
+/**
+ * Created by obiero on 5/7/2015.
+ */
+public interface DrugDao {
+    public int addDrug(Drug drug);
+    public void updateDrug(Drug drug);
+    public List<Drug> listDrug();
+    public Drug getDrugById(int id);
+    public void removeDrug(int id);
+}
